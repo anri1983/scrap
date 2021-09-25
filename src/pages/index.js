55ef4c676adbe57391f2c3af85f9832481899309
anri1983 <31101983@rambler.ru>
2021-09-26 00:01:10 +0300

    addPlugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import * as React from "react";
-import {message} from './index.module.css';
+//import {message} from './index.module.css';
+import { StaticImage } from 'gatsby-plugin-image';
+//import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 function IndexPage (){
   return(
-<div className = {message} >Under reconstruction!</div>
+<div>
+  <Layout pageTitle="Home Page">
+  <StaticImage
+  alt = "Nature"
+  src = "https://sun9-76.userapi.com/impf/c629526/v629526151/9742/ohfJXzKHE0U.jpg?size=640x427&quality=96&sign=b6b4a49e860abddc345204adb833afaa&type=album"/>
+  </Layout>
+</div>
   )
 }
 
